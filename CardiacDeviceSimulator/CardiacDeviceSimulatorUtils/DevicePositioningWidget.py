c5740230ca747da156cc98254b9a9ce9d00263f6
Christian Herz <christianherz1985@gmail.com>
2020-02-11 16:37:57 -0500

    ENH: added option to place centerline in DevicePositioningWidget
    - reorganized vessel part and centerline part into separate groupboxes
      in order to hide them
    
    issue: https://github.com/JolleyLabCHOP/Internal/issues/22

--- CardiacDeviceSimulator/CardiacDeviceSimulatorUtils/DevicePositioningWidget.py
+++ CardiacDeviceSimulator/CardiacDeviceSimulatorUtils/DevicePositioningWidget.py
@@ -8,6 +8,15 @@ class DevicePositioningWidget(DeviceWidget):
   """
   """
 
+  @staticmethod
+  def createHLayout(elements):
+    widget = qt.QWidget()
+    rowLayout = qt.QHBoxLayout()
+    widget.setLayout(rowLayout)
+    for element in elements:
+      rowLayout.addWidget(element)
+    return widget
+
   def __init__(self, parent=None):
     DeviceWidget.__init__(self, parent)
     self.observedParameterNodeEvents = [vtk.vtkCommand.ModifiedEvent]
@@ -18,57 +27,75 @@ class DevicePositioningWidget(DeviceWidget):
 
     lay = self.layout()
 
+    self.vesselGroupBox = qt.QGroupBox("Vessel lumen")
+    self.vesselGroupBox.setLayout(qt.QFormLayout())
+
+    # vessel lumen
     self.vesselSegmentSelector = slicer.qMRMLSegmentSelectorWidget()
     self.vesselSegmentSelector.setMRMLScene(slicer.mrmlScene)
     self.vesselSegmentSelector.setToolTip("Select vessel lumen (blood) segment. The segment must be a solid, filled "
                                           "model of the blood (not a shell).")
-    lay.addRow("Vessel lumen: ", self.vesselSegmentSelector)
+    self.vesselGroupBox.layout().addRow("Segmentation Node: ", self.vesselSegmentSelector)
     self.vesselSegmentSelector.connect("currentNodeChanged(vtkMRMLNode*)", self.onVesselLumenSegmentSelectionChanged)
     self.vesselSegmentSelector.connect("currentSegmentChanged(QString)", self.onVesselLumenSegmentSelectionChanged)
 
     # Button to position device at closest centerline point
     self.processVesselSegmentButton = qt.QPushButton("Detect vessel centerline")
     self.processVesselSegmentButton.setToolTip("Extract centerline and lumen surface from vessel segment")
-    lay.addRow(self.processVesselSegmentButton)
+    self.vesselGroupBox.layout().addRow(self.processVesselSegmentButton)
     self.processVesselSegmentButton.connect('clicked(bool)', self.onProcessVesselSegmentClicked)
     self.processVesselSegmentButton.enabled = bool(self.vesselSegmentSelector.currentSegmentID())
 
+    lay.addRow(self.vesselGroupBox)
 
     # Centerline curve node selector
+
+    self.centerlineGroupBox = qt.QGroupBox("Centerline")
+    self.centerlineGroupBox.setLayout(qt.QFormLayout())
+
     self.centerlineNodeSelector = slicer.qMRMLNodeComboBox()
     self.centerlineNodeSelector.nodeTypes = ["vtkMRMLMarkupsCurveNode"]
     self.centerlineNodeSelector.setNodeTypeLabel("Centerline", "vtkMRMLMarkupsCurveNode")
     self.centerlineNodeSelector.baseName = "Centerline"
-    self.centerlineNodeSelector.selectNodeUponCreation = False
-    self.centerlineNodeSelector.addEnabled = False
-    self.centerlineNodeSelector.removeEnabled = False
+    self.centerlineNodeSelector.selectNodeUponCreation = True
+    self.centerlineNodeSelector.addEnabled = True
+    self.centerlineNodeSelector.removeEnabled = True
     self.centerlineNodeSelector.noneEnabled = True
     self.centerlineNodeSelector.showHidden = True
     self.centerlineNodeSelector.renameEnabled = True
     self.centerlineNodeSelector.setMRMLScene(slicer.mrmlScene)
     self.centerlineNodeSelector.setToolTip("Select a curve to be used for positioning the device")
-    lay.addRow("Centerline: ", self.centerlineNodeSelector)
     self.centerlineNodeSelector.connect("currentNodeChanged(vtkMRMLNode*)",
                                              self.onCenterlineNodeSelectionChanged)
 
+    self.centerlinePlaceWidget = slicer.qSlicerMarkupsPlaceWidget()
+    self.centerlinePlaceWidget.setMRMLScene(slicer.mrmlScene)
+
+    self.centerLineHBox = self.createHLayout([qt.QLabel("Centerline curve: "), self.centerlineNodeSelector,
+                                              self.centerlinePlaceWidget])
+
+    self.centerlineGroupBox.layout().addRow(self.centerLineHBox)
+
     # Button to position device at closest centerline point
     self.positionDeviceAtCenterlineButton = qt.QPushButton("Position device at centerline")
-    lay.addRow(self.positionDeviceAtCenterlineButton)
+    self.centerlineGroupBox.layout().addRow(self.positionDeviceAtCenterlineButton)
     self.positionDeviceAtCenterlineButton.connect('clicked(bool)', self.onPositionDeviceAtCenterlineClicked)
     self.positionDeviceAtCenterlineButton.enabled = (self.centerlineNodeSelector.currentNode() is not None)
 
     # Button to flip device orientation
     self.flipDeviceOrientationButton = qt.QPushButton("Flip device orientation")
-    lay.addRow(self.flipDeviceOrientationButton)
+    self.centerlineGroupBox.layout().addRow(self.flipDeviceOrientationButton)
     self.flipDeviceOrientationButton.connect('clicked(bool)', self.onFlipDeviceOrientationClicked)
     self.flipDeviceOrientationButton.enabled = (self.centerlineNodeSelector.currentNode() is not None)
 
     # Pick centerline model
     self.moveDeviceAlongCenterlineSliderWidget = UIHelper.addSlider({
       "name": "Move device along centerline:", "info": "Moves the device along extracted centerline", "value": 0,
-      "unit": "", "minimum": 0, "maximum": 100, "singleStep": 0.1, "pageStep": 1, "decimals": 1}, lay,
-      self.onMoveDeviceAlongCenterlineSliderChanged)
+      "unit": "", "minimum": 0, "maximum": 100, "singleStep": 0.1, "pageStep": 1, "decimals": 1},
+      self.centerlineGroupBox.layout(), self.onMoveDeviceAlongCenterlineSliderChanged)
     self.moveDeviceAlongCenterlineSliderWidget.enabled = (self.centerlineNodeSelector.currentNode() is not None)
+
+    lay.addRow(self.centerlineGroupBox)
 
     # Position
     self.devicePositioningPositionSliderWidget = slicer.qMRMLTransformSliders()
@@ -115,6 +142,7 @@ class DevicePositioningWidget(DeviceWidget):
     self.vesselSegmentSelector.setCurrentSegmentID(vesselLumenSegmentId)
 
   def onCenterlineNodeSelectionChanged(self):
+    self.centerlinePlaceWidget.setCurrentNode(self.centerlineNodeSelector.currentNode())
     self.logic.setCenterlineNode(self.centerlineNodeSelector.currentNode())
 
   def onProcessVesselSegmentClicked(self):
