418888c3ab6ffc175f8b83672accb49486a690a1
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2023-07-10 18:31:37 -0400

    BUG: Ensure UltrasoundImage3dReader sets DICOM.instanceUIDs to table & color nodes
    This commit ensures that the table and color nodes are also associated
    with the corresponding "DICOM.instanceUIDs"

--- UltrasoundImage3dReader/UltrasoundImage3dReader.py
+++ UltrasoundImage3dReader/UltrasoundImage3dReader.py
@@ -191,6 +191,7 @@ class UltrasoundImage3dReaderFileReader(object):
       # retrieve ECG info
       ecg = source.GetECG()
       tableNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLTableNode", baseName+" ECG")
+      tableNode.SetAttribute("DICOM.instanceUIDs", slicer.dicomDatabase.instanceForFile(filePath))
       ecgSamples = self.safeArrayToNumpy(ecg.samples)
       numberOfSamples = len(ecgSamples)
       ecgTimestamps = np.arange(ecg.start_time, ecg.start_time+numberOfSamples*ecg.delta_time, ecg.delta_time)
@@ -208,6 +209,7 @@ class UltrasoundImage3dReaderFileReader(object):
 
       # set color table
       colorTableNode = slicer.mrmlScene.AddNewNodeByClass("vtkMRMLColorTableNode", baseName+" colormap")
+      colorTableNode.SetAttribute("DICOM.instanceUIDs", slicer.dicomDatabase.instanceForFile(filePath))
       colorMapArray = source.GetColorMap()
       numberOfColors = len(colorMapArray)
       colorTableNode.SetTypeToUser()
